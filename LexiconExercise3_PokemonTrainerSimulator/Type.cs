9caff2a62379f977491a41cec024ee0e544637d8
a21jeaha <hisui86@gmail.com>
2025-05-29 11:49:38 +0200

    created an enum representing the different types attack or pokemons can have

---
+++ LexiconExercise3_PokemonTrainerSimulator/Type.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconExercise3_PokemonTrainerSimulator
+{
+	/// <summary>
+	/// An enumeration representing different types. 
+	/// Can be used for both Pokemons and Attacks.
+	/// </summary>
+	internal enum Type
+	{
+		Fire,
+		Water,
+		Electric
+	}
+}
