cb4723d92611b92f51c1ef009de4861bb4a18a8e
a21jeaha <hisui86@gmail.com>
2025-05-29 13:24:23 +0200

    added I & NameValidator, it validates names

---
+++ LexiconExercise3_PokemonTrainerSimulator/Pokemon/Pokemon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LexiconExercise3_PokemonTrainerSimulator.Utils.ConsoleAbstraction;
+
+
+namespace LexiconExercise3_PokemonTrainerSimulator
+{
+	internal abstract class Pokemon
+	{
+		private readonly ConsoleWritePrint _consoleWritePrint;
+		private string _name;
+
+		public string Name 
+		{ 
+			get => _name;
+			set => _name = ValidateNewName();
+		}
+
+		public Pokemon(ConsoleWritePrint consoleWritePrint)
+		{
+			_consoleWritePrint = consoleWritePrint;
+		}
+
+		private string ValidateNewName()
+		{
+			string pokemonName = _consoleWritePrint.ReadLine();
+
+			return pokemonName;
+		}
+	}
+}
---
+++ LexiconExercise3_PokemonTrainerSimulator/Utils/NameValidator/NameValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconExercise3_PokemonTrainerSimulator.Utils.NameValidator
+{
+	/// <summary>
+	/// A class that validates Pokémon names based on specific criteria.
+	/// </summary>
+	internal class NameValidator : INameValidator
+	{
+		/// <inheritdoc/>
+		public bool IsNameValid(string name)
+		{
+			return !string.IsNullOrWhiteSpace(name) &&
+				   (name.Length >= 2) &&
+				   (name.Length <= 15); 
+		}
+	}
+}
