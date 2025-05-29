a628ec2789b27d0d50a8c97bcfc0204e505adbf5
a21jeaha <hisui86@gmail.com>
2025-05-29 15:13:02 +0200

    created three generic pokemon types, Fire, Water, Electric. A basic attack method was implemented

---
+++ LexiconExercise3_PokemonTrainerSimulator/Pokemon/GenericTypePokemon/ElectricPokemon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconExercise3_PokemonTrainerSimulator.Pokemon
+{
+	internal class ElectricPokemon : Pokemon
+	{
+		public ElectricPokemon(string name, int level, Type type, List<Attack> attacks) 
+			: base(name, level, type, attacks)
+		{
+		}
+
+		/// <summary>
+		/// Basic version of the Attack method. Simply uses the first attack from the Attacks list.
+		/// </summary>
+		/// <returns>A string representing the action of attacking.</returns>
+		public override string Attack() => 
+			Attacks.FirstOrDefault()
+				.Use(Level);
+	}
+}
---
+++ LexiconExercise3_PokemonTrainerSimulator/Pokemon/GenericTypePokemon/FirePokemon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconExercise3_PokemonTrainerSimulator.Pokemon.GenericTypePokemon
+{
+	internal class FirePokemon : Pokemon
+	{
+
+		public FirePokemon(string name, int level, List<Attack> attacks) 
+			: base(name, level, Type.Fire, attacks)
+		{
+
+		}
+
+		/// <summary>
+		/// Basic version of the Attack method. Simply uses the first attack from the Attacks list.
+		/// </summary>
+		/// <returns>A string representing the action of attacking.</returns>
+		public override string Attack() =>
+			Attacks.FirstOrDefault()
+				.Use(Level);
+	}
+}
