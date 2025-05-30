7cfcf7c8c41a4b111fdc8deb56ec533079314941
a21jeaha <hisui86@gmail.com>
2025-05-30 15:39:58 +0200

    created an interface for evolving Pokemon

---
+++ LexiconExercise3_PokemonTrainerSimulator/Pokemon/IEvolvable.cs
@@ -0,0 +1,13 @@
+﻿namespace LexiconExercise3_PokemonTrainerSimulator.Pokemon
+{
+	/// <summary>
+	/// An interface representing a <see cref="Pokemon"/> that can evolve into a more powerful form.
+	/// </summary>
+	internal interface IEvolvable
+	{
+		/// <summary>
+		/// Evolves the pokemon into its next stage, if applicable.
+		/// </summary>
+		void Evolve();
+	}
+}
