697aa6b6156f53826c855aacae4de2edacc2d84b
a21jeaha <hisui86@gmail.com>
2025-05-30 09:54:55 +0200

    created child class Charmander which inherits from generic FirePokemon class

---
+++ LexiconExercise3_PokemonTrainerSimulator/Pokemon/NamedTypePokemon/Charmander.cs
@@ -0,0 +1,16 @@
+﻿using LexiconExercise3_PokemonTrainerSimulator.Pokemon.GenericTypePokemon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconExercise3_PokemonTrainerSimulator.Pokemon.NamedTypePokemon;
+
+internal class Charmander : FirePokemon
+{
+	public Charmander(string name, int level, List<Attack> attacks) 
+		: base(name, level, attacks)
+	{
+	}
+}
